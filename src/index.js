2e7c6d69a825ed829c7bb0541415432fe3548255
jnanda <jyoti.kaustuv.nanda@gmail.com>
2017-10-08 05:28:42 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,57 +1,65 @@
 import { h, Component } from 'preact';
-import { AtomDesignInputField, VaultDesignInputField } from '@jsfoobar/design-patterns';
+import { VaultDesignInputField } from '@jsfoobar/design-patterns';
 import InputField from '@jsfoobar/input-field';
 import CardNumberMasking from './js/components/card-number-masking';
 
 const renderCardNumber = (MaskingComponent) => {
-  class CardNumber extends Component {
-    constructor(props) {
-      super(props);
+	class CardNumber extends Component {
+		onKeyUpHandler = e => {
+			const inputState = CardNumber.maskingOnKeyUp(e, this.props);
 
-      this.state = {
-        value: '',
-        mask: props.placeholder
-      };
+			if (inputState && (inputState.value || inputState.mask)) {
+				this.setState({
+					value: inputState.value,
+					mask: inputState.mask
+				});
+			}
+		}
 
-      this.onKeyUpHandler = this.onKeyUpHandler.bind(this);
-    }
+		constructor(props) {
+			super(props);
 
-    onKeyUpHandler = e => {
-      const inputState = CardNumber.maskingOnKeyUp(e, this.props);
+			this.state = {
+				value: '',
+				mask: props.placeholder
+			};
 
-      if (inputState && (inputState.value || inputState.mask)) {
-        this.setState({
-          value: inputState.value,
-          mask: inputState.mask
-        });
-      }
-    }
+			this.onKeyUpHandler = this.onKeyUpHandler.bind(this);
+		}
 
-    render(props) {
-      const { label, ...propsForInput } = props; // eslint-disable-line no-unused-vars
+		render(props) {
+			const { label, ...propsForInput } = props; // eslint-disable-line no-unused-vars
 
-      propsForInput.maxLength = props.placeholder.length;
-      propsForInput.mask = this.state.mask;
-      propsForInput.value = this.state.value;
+			propsForInput.maxLength = props.placeholder.length;
+			propsForInput.mask = this.state.mask;
+			propsForInput.value = this.state.value;
 
-      return (
-        <VaultDesignInputField label={label}>
-          <InputField {...propsForInput} onKeyUp={this.onKeyUpHandler} value={this.state.value} />
-          <MaskingComponent {...propsForInput} />
-        </VaultDesignInputField>
-      )
-    }
-  };
+			return (
+				<VaultDesignInputField label={label}>
+					<InputField {...propsForInput} onKeyUp={this.onKeyUpHandler} value={this.state.value} />
+					<MaskingComponent {...propsForInput} />
+				</VaultDesignInputField>
+			);
+		}
+	}
 
-  CardNumber.maskingOnKeyUp = MaskingComponent.maskingOnKeyUp;
+	CardNumber.maskingOnKeyUp = MaskingComponent.maskingOnKeyUp;
 
-  return CardNumber;
-}
+	return CardNumber;
+};
 
 const CardNumber = renderCardNumber(CardNumberMasking);
 
 const App = () => (
-  <CardNumber label="Card Number" class="masked" type="tel" name="card-number" id="cardNumber" placeholder="XXXX XXXX XXXX XXXX" required="required" />
+	<CardNumber
+		label="Card Number"
+		class="masked"
+		type="tel"
+		name="card-number"
+		id="cardNumber"
+		placeholder="XXXX XXXX XXXX XXXX"
+		required="required"
+	/>
 );
 
 export default App;
